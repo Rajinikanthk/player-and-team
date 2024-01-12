f5459d361bb4b63aabe17e33cb8f415232a4b46d
raj <raj@gmail.com>
2024-01-12 12:40:26 +0530

    Add project files.

---
+++ player and team/ITeam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace player_and_team
+{
+    public interface ITeam
+    {
+        void AddPlayer(Player player);
+        void RemovePlayer(int playerId);
+        Player GetPlayerById(int playerId);
+        List<Player> GetPlayersByName(string playerName);
+        List<Player> GetAllPlayers();
+    }
+}
